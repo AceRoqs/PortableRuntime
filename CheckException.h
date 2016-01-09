889857e6e6f2189568878815b654d5f672e6a8cf
Toby Jones <aceroqs@users.noreply.github.com>
2016-01-09 01:08:26 -0600

    Pass string instead of raw buffer to exception.  Add detail namespace.

--- CheckException.h
+++ CheckException.h
@@ -3,9 +3,13 @@
 namespace PortableRuntime
 {
 
+namespace Detail
+{
+
 // TODO: Move implementation to .cpp file.
-// TODO: Move this to a detail namespace, and have client code use the macro to avoid unnecessary evaluaton of string.
-inline void check_exception(bool result, _In_z_ const char* message)
+// TODO: Add logging.  TODO: Consider DebugBreak in log?
+// TODO: Add logging in catch statements to pair with the throws?  i.e. "Recovered exception, etc."
+inline void check_exception(bool result, const std::string& message)
 {
     if(!result)
     {
@@ -18,15 +22,19 @@ inline void check_exception(bool result, _In_z_ const char* message)
 #endif
         // Force break.
         assert(false);
-        throw std::exception(message);
+        throw std::exception(message.c_str());
     }
 }
 
+}
+
+// CHECK_EXCEPTION macro allows for usage of __FILE__ and __LINE__
+// and avoid evaluation of string in success case.
 #define CHECK_EXCEPTION(zzz_expr, zzz_str) { \
     const bool zzz_val = (zzz_expr); \
     if(!zzz_val) \
     { \
-        PortableRuntime::check_exception(zzz_val, (zzz_str)); \
+        PortableRuntime::Detail::check_exception(zzz_val, (zzz_str)); \
     } \
     _Analysis_assume_(zzz_expr); \
 }
