2124d7de68f1dc6867e454f75c6f6a2baf3b3279
Toby Jones <aceroqs@users.noreply.github.com>
2016-01-10 08:56:38 -0800

    Some idiot doesn't know how his own code works.

--- CheckException.h
+++ CheckException.h
@@ -12,11 +12,11 @@ void throw_exception(const std::string& message, _In_z_ const char* file_name, i
 
 // CHECK_EXCEPTION macro allows for usage of __FILE__ and __LINE__
 // and avoid evaluation of string in success case.
-#define CHECK_EXCEPTION(zzz_expr, zzz_str) { \
+#define CHECK_EXCEPTION(zzz_expr, zzz_message) { \
     const bool zzz_val = (zzz_expr); \
     if(!zzz_val) \
     { \
-        PortableRuntime::Detail::throw_exception((zzz_str), __FILE__, __LINE__); \
+        PortableRuntime::Detail::throw_exception((zzz_message), __FILE__, __LINE__); \
     } \
     _Analysis_assume_((zzz_expr)); \
 }
