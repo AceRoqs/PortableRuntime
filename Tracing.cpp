e4878eb0bdfde13e20d479fdd79ddc342db1d1a0
Toby Jones <ace_roqs@yahoo.com>
2016-09-01 21:34:41 -0700

    Refactor to potentially allow release traces.

--- Tracing.cpp
+++ Tracing.cpp
@@ -4,9 +4,10 @@
 namespace PortableRuntime
 {
 
-static void null_dprintf(_In_z_ const char* format) noexcept
+static void null_dprintf(_In_z_ const char* format, va_list args) noexcept
 {
-    (void)format;   // Unreferenced parameter.
+    (void)format;   // Unreferenced parameters.
+    (void)args;
 }
 
 static Output_dprintf output_dprintf = null_dprintf;
@@ -14,18 +15,12 @@ static Output_dprintf output_dprintf = null_dprintf;
 _Use_decl_annotations_
 void dprintf(const char* format, ...) noexcept
 {
-#ifndef NDEBUG
-    char print_buffer[1024];
     va_list args;
     va_start(args, format);
 
-    vsprintf(print_buffer, format, args);
+    output_dprintf(format, args);
 
     va_end(args);
-    output_dprintf(print_buffer);
-#else
-    (void)format;   // Unreferenced parameter.
-#endif
 }
 
 void set_dprintf(const Output_dprintf output_function) noexcept
@@ -38,6 +33,15 @@ void set_dprintf(const Output_dprintf output_function) noexcept
     {
         output_dprintf = output_function;
     }
+}
+
+_Use_decl_annotations_
+std::string dprintf_string_from_varargs(const char* format, va_list args) noexcept
+{
+    char print_buffer[1024];
+    vsprintf_s(print_buffer, format, args);
+
+    return print_buffer;
 }
 
 }
--- Tracing.h
+++ Tracing.h
@@ -3,12 +3,13 @@
 namespace PortableRuntime
 {
 
-typedef void(*Output_dprintf)(_In_z_ const char* format) /* noexcept */;
+typedef void(*Output_dprintf)(_In_z_ const char* format, va_list args) /* noexcept */;
 
 void dprintf(_In_z_ const char* format, ...) noexcept;
 
 // TODO: Make this a class, which can restore the previous dprintf on scope exit?
 void set_dprintf(const Output_dprintf output_function) noexcept;
+std::string dprintf_string_from_varargs(_In_z_ const char* format, va_list args) noexcept;
 
 }
 
