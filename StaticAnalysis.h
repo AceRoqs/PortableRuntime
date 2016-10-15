f7e801797407930318d07e3a41dfa4e68ac9f966
Toby Jones <ace_roqs@yahoo.com>
2016-10-14 18:51:25 -0700

    Add SAL as required by CrappyCrypto.

--- StaticAnalysis.h
+++ StaticAnalysis.h
@@ -2,7 +2,13 @@
 
 #ifndef _MSC_VER
 
+#define _In_
 #define _In_z_
+#define _In_reads_(x)
+#define _Inout_updates_(x)
+#define _Inout_updates_all_(x)
+#define _Out_writes_(x)
+#define _Out_writes_all_(x)
 #define _Use_decl_annotations_
 
 #endif
