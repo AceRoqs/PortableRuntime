347771aba46808b3b877bbeb99af8cd0757b30bf
Toby Jones <ace_roqs@yahoo.com>
2016-11-21 08:48:40 -0800

    Clang SAL additions for ImageProcessing.

--- StaticAnalysis.h
+++ StaticAnalysis.h
@@ -9,10 +9,14 @@
 #define _In_
 #define _In_z_
 #define _In_reads_(x)
+#define _In_reads_opt_(x)
+#define _In_reads_to_ptr_(x)
 #define _Inout_updates_(x)
 #define _Inout_updates_all_(x)
+#define _Out_
 #define _Out_writes_(x)
 #define _Out_writes_all_(x)
+#define _Out_writes_to_ptr_(x)
 #define _Use_decl_annotations_
 
 #endif
